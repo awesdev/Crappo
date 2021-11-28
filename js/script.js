79303d3b88cfc44420c41285ce9999247198fdfe
Kirill <kirillkolesnichenko21@gmail.com>
2021-11-28 11:44:46 +0300

    end.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
     new WOW().init();
-
-    $(".burger-menu").on("click", function () {
-        $(this).toggleClass("burger-menu--active");
-        $(".header__menu").toggleClass("header__menu--active");
-        $(".header__menu").toggleClass("wow fadeInRight");
-    })
 })
